39b20c3938e27bede97bb14c9230cbce9404ec64
grian <grianguy32@gmail.com>
2025-06-15 03:25:40 +0300

    feat: write, quit, rc, byte, str

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-network:3.1.3")
+    implementation("io.ktor:ktor-server-netty:3.1.3")
+    implementation("io.ktor:ktor-server-core-jvm:3.1.3")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,56 @@
 package me.grian
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import io.netty.buffer.Unpooled
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.system.exitProcess
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+suspend fun main(args: Array<String>) {
+    val selectorManager = SelectorManager(Dispatchers.IO)
+    var socket = aSocket(selectorManager).tcp().connect(args[0], args[1].toInt())
+
+    var writeChannel = socket.openWriteChannel(autoFlush = true)
+
+    val sc = Scanner(System.`in`)
+    val bytes = Unpooled.buffer()
+
+    while (sc.hasNextLine()) {
+        val next = sc.nextLine()
+
+        when {
+            next == "q" -> exitProcess(0)
+            next == "w" -> {
+                writeChannel.writeFully(bytes.nioBuffer())
+                println("sent: ${bytes.nioBuffer()}")
+                bytes.clear()
+            }
+            next == "rc" -> {
+                withContext(Dispatchers.IO) {
+                    socket.close()
+                }
+                socket = aSocket(selectorManager).tcp().connect(args[0], args[1].toInt())
+                writeChannel = socket.openWriteChannel(autoFlush = true)
+                bytes.clear()
+            }
+            next.startsWith("byte") -> {
+                val value = next.split(" ")[1].removePrefix("0x").toByte()
+                bytes.writeByte(value.toInt())
+            }
+            next.startsWith("str") -> {
+                val value = next.split(" ").drop(1).joinToString(" ")
+                bytes.writeInt(value.length)
+                println(value.toByteArray().toList())
+                bytes.writeBytes(value.toByteArray())
+            }
+        }
+    }
+
+    withContext(Dispatchers.IO) {
+        socket.close()
+        selectorManager.close()
     }
 }
